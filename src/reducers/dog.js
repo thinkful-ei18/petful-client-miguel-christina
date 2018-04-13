c8bfe466e96eeceb259505ce02dab58e2a4b0a58
emptyelephants <emptyelephants@gmail.com>
2018-04-13 11:15:03 -0700

    removed comments from dog reducers

--- src/reducers/dog.js
+++ src/reducers/dog.js
@@ -1,19 +1,4 @@
 import {FETCH_DOG_SUCCESS, FETCH_DOG_REQUEST, FETCH_DOG_ERROR} from '../actions/dog.js';
-// dummy state
-// const initialState = {
-//   currentDog:{
-//     imageURL: 'https://www.cesarsway.com/sites/newcesarsway/files/styles/large_article_preview/public/Common-dog-behaviors-explained.jpg?itok=FSzwbBoihttps://www.cesarsway.com/sites/newcesarsway/files/styles/large_article_preview/public/Common-dog-behaviors-explained.jpg?itok=FSzwbBoi',
-//     imageDescription: 'A dog tilting it\'s head out of curiosity',
-//     name: 'Jack',
-//     sex: 'Female',
-//     age: 5,
-//     breed: 'Australian Shepard',
-//     story: 'Rescued from an earthquake.'
-//   },
-//   loading:false,
-//   error:null,
-// }
-// real state
 const initialState = {
   currentDog: null,
   loading:false,
